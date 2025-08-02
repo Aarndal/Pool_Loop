4233a76ef9508183c52a070260e4142e95a81043
leniv <leniv@HURI>
2025-08-02 18:40:46 +0200

    Animation

--- Animation.cpp
+++ Animation.cpp
@@ -1,6 +1,42 @@
 #include "Animation.h"
 
-void Animation::draw(olc::vf2d pos)
+namespace
 {
+	olc::Renderable loadImage(const std::filesystem::path& path)
+	{
+		olc::Renderable renderable;
+		renderable.Load(path.string());
+		return renderable;
+	}
 
+	std::vector<olc::Renderable> loadImages(const std::vector<std::filesystem::path>& vecImagePaths)
+	{
+		std::vector<olc::Renderable> renderables;
+		std::ranges::transform(vecImagePaths, std::back_inserter(renderables), loadImage);
+		return renderables;
+	}
+}
+
+Animation::Animation(const std::vector<std::filesystem::path>& vecImagePaths, float frameTime) 
+	: m_vecImages{ loadImages(vecImagePaths) }
+	, m_frameTime{ frameTime }
+{
+}
+
+void Animation::draw(olc::vf2d pos, float fElepsedTime, olc::PixelGameEngine* pge)
+{
+	m_currentImageIndex = updateIndex(m_currentImageIndex, fElepsedTime);
+
+	pge->DrawRotatedDecal(pos, m_vecImages[m_currentImageIndex].Decal(), 0.f);
+}
+
+int Animation::updateIndex(int currentIndex, float fElepsedTime)
+{
+	m_time += fElepsedTime;
+	if (m_time >= m_frameTime)
+	{
+		const int newIndex = ++currentIndex;
+		return newIndex == m_vecImages.size() ? 0 : newIndex;
+	}
+	return currentIndex;
 }
--- Animation.h
+++ Animation.h
@@ -2,7 +2,16 @@
 #include <olcPixelGameEngine.h>
 class Animation
 {
+public:
+	Animation(const std::vector<std::filesystem::path>& vecImagePaths, float frameTime);
+	void draw(olc::vf2d pos, float fElepsedTime, olc::PixelGameEngine* pge);
 
-	void draw(olc::vf2d pos);
+private:
+	int updateIndex(int currentIndex, float fElepsedTime);
+
+	std::vector<olc::Renderable> m_vecImages;
+	int m_currentImageIndex{ 0 };
+	float m_frameTime{1.f};
+	float m_time{0.f};
 };
 
