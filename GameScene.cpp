d02ae79c9fde7392f869cf3768b4775d18871052
Aaron Dahlhauser <aarndal@duck.com>
2025-08-02 18:46:21 +0200

    Added UserInput and Gravity

--- GameScene.cpp
+++ GameScene.cpp
@@ -1,16 +1,43 @@
 #include "GameScene.h"
 
-GameScene::GameScene(olc::PixelGameEngine* engine) : m_engine{engine}
+GameScene::GameScene(olc::PixelGameEngine* engine) : m_engine{ engine }
 {
 }
 
 void GameScene::init()
 {
+	m_falling = false;
+	m_gravitySpeed = 10.0f;
+	m_speed = 2.0f;
 }
 
-void GameScene::update(float time)
+void GameScene::update(float elapsedTime)
 {
-	olc::vf2d pos{ 500.f,300.f };
+	m_engine->Clear(olc::VERY_DARK_BLUE);
+
+	if (m_falling)
+	{
+		if (m_engine->GetKey(olc::Key::A).bHeld)
+		{
+			m_pos += olc::vf2d{ -1,0 } *m_speed * elapsedTime;
+		}
+
+		if (m_engine->GetKey(olc::Key::D).bHeld)
+		{
+			m_pos += olc::vf2d{ 1,0 } *m_speed * elapsedTime;
+		}
+
+		m_pos += olc::vf2d{ 0,1 } *m_gravitySpeed * elapsedTime;
+	}
+
+	if (!m_falling)
+	{
+		if (m_engine->GetKey(olc::Key::SPACE).bPressed)
+		{
+			m_falling = true;
+		}
+	}
+
 	olc::vf2d size{ 400.f,50.f };
-	m_engine->DrawRectDecal(pos, size, olc::RED);
+	m_engine->DrawRectDecal(m_pos, size, olc::RED);
 }
--- GameScene.h
+++ GameScene.h
@@ -8,8 +8,12 @@ class GameScene : public IScene
 public:
 	GameScene(olc::PixelGameEngine* engine);
 	void init() override;
-	void update(float time) override;
+	void update(float elapsedTime) override;
 
 private:
-	olc::PixelGameEngine* m_engine;
+	olc::PixelGameEngine* m_engine{};
+	olc::vf2d m_pos{ 500.f,300.f };
+	bool m_falling{ false };
+	float m_gravitySpeed{ 0.0f };
+	float m_speed{ 0.0f };
 };
