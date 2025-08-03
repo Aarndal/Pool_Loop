e74e49cf15574af0def8b1de1810a0c1cb9c675f
Fabian Weber <>
2025-08-03 14:26:39 +0200

    logo (2)

--- Logo.cpp
+++ Logo.cpp
@@ -2,10 +2,10 @@
 
 
 Logo::Logo(olc::vf2d pos) :
-	L{ pos,{} },
-	O_1{ pos + olc::vf2d{300.f,0},{} },
-	O_2{ pos + olc::vf2d{600.f,0},{} },
-	P{ pos + olc::vf2d{900.f,0},{} }
+	L{ .pos = pos,.posMax = pos + olc::vf2d{600.f,0},.posMin = pos,.dir = {1.f,0.f},.image = {} },
+	O_1{ .pos = pos + olc::vf2d{200.f,0},.posMax = pos + olc::vf2d{600.f,0},.posMin = pos ,.dir = {0.f,0.f},.image = {} },
+	O_2{ .pos = pos + olc::vf2d{400.f,0},.posMax = pos + olc::vf2d{600.f,0},.posMin = pos ,.dir = {0.f,0.f},.image = {} },
+	P{ .pos = pos + olc::vf2d{600.f,0},.posMax = pos + olc::vf2d{600.f,0},.posMin = pos ,.dir = {-1.f,0.f},.image = {} }
 {
 	L.image.Load("resources\\Title_L.png");
 	O_1.image.Load("resources\\Title_O_1.png");
@@ -13,9 +13,12 @@ Logo::Logo(olc::vf2d pos) :
 	P.image.Load("resources\\Title_P.png");
 }
 
-void Logo::update(float /*fDeltaTime*/)
+void Logo::update(float fDeltaTime)
 {
-
+	updateLetter(fDeltaTime,L);
+	updateLetter(fDeltaTime, O_1);
+	updateLetter(fDeltaTime, O_2);
+	updateLetter(fDeltaTime, P);
 }
 
 
@@ -24,11 +27,25 @@ void Logo::drawLetter(olc::PixelGameEngine* pge, const Logo::Letter& letter)
 	pge->DrawDecal(letter.pos, letter.image.Decal());
 }
 
+void Logo::updateLetter(float fDeltaTime, Logo::Letter& letter)
+{
+	const float speed = 100.f;
+	letter.pos += letter.dir * fDeltaTime * speed;
+	if (letter.pos.x < letter.posMin.x)
+	{
+		letter.dir = { 1.f,0.f };
+	}
+	if (letter.pos.x > letter.posMax.x)
+	{
+		letter.dir = { -1.f,0.f };
+	}
+}
+
 
 void Logo::draw(olc::PixelGameEngine* pge)
 {
-	drawLetter(pge, L);
-	drawLetter(pge, O_1);
+	drawLetter(pge, P);	
 	drawLetter(pge, O_2);
-	drawLetter(pge, P);
+	drawLetter(pge, O_1);
+	drawLetter(pge, L);
 }
